a35d0638678bb2cf23904d5a8d3217afce8cad1c
Colin <beutt102@mail.chapman.edu>
2021-06-28 10:56:08 -0700

    updatelt

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let nums =[0,1,0,1,8]
+
+let nums1 = [1,2,2,1]
+
+let nums2 = [2,2]
+
+let s = 'leetcode'
+
+const firstUnique = str =>{
+    const l =s.split('')
+    let objectB = {}
+}
+
+
+
+console.log(mapS)
+
+
+// let xValue = '123'
+
+// const reverseString = (str) =>{
+//    return [...str].reverse().join('')
+// }
+
+// console.log(reverseString(xValue))
+
+// loop array, if zeros push to the end
+
+// const zerosBack = function(arr){
+
+// for(let i =0; i < arr.length; i++ ){
+//     if (arr[i] == 0){
+//         arr.splice(i,1);
+//         arr.push(0)
+//     }
+// }
+// return arr
+// }
+// //
+// console.log(zerosBack(nums))
+
+
+// const twoSum = function(arr){
+//     let sums =[]
+//     for(let i =0; i < arr.length; i++){
+//             if(i == 1){
+//                 sums.push(i)
+                
+//         }
+//     }
+//     console.log(sums) 
+// }
+
+// twoSum(nums)
+
