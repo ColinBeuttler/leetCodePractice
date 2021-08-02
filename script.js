454fd64272545ce6f98e26a8c776763f37afd6ad
Colin <beutt102@mail.chapman.edu>
2021-08-02 11:08:41 -0700

    newproblem

--- script.js
+++ script.js
@@ -16,6 +16,14 @@ let t = 'hifi';
 
 let prices = [1, 2, 3, 4, 5, 6, 7, 2]
 
+// Input: n = 5, bad = 4
+// Output: 4
+// Explanation:
+// call isBadVersion(3) -> false
+// call isBadVersion(5) -> true
+// call isBadVersion(4) -> true
+// Then 4 is the first bad version.
+
 const mergeSortArr = (arr1, arr2) =>{
   let arr3 = arr1.concat(arr2)
   arr3.sort()
