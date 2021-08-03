a9bfc8b5682b1d1b237108bde31dbed3c4df49b7
Colin <beutt102@mail.chapman.edu>
2021-08-03 11:39:20 -0700

    carproto

--- script.js
+++ script.js
@@ -16,23 +16,23 @@ let t = 'hifi';
 
 // let prices = [1, 2, 3, 4, 5, 6, 7, 2]
 
-const Car =function(make, speed){
-  this.make = make
-  this.speed = speed
-}
+// const Car =function(make, speed){
+//   this.make = make
+//   this.speed = speed
+// }
 
-const car1 =new Car ('Bmw', 120)
-const car2 = new Car('Mercedes', 95)
+// const car1 =new Car ('Bmw', 120)
+// const car2 = new Car('Mercedes', 95)
 
-Car.prototype.accelerate = function(){
-  this.speed +=10
-  console.log(`${this.speed}`)
-}
+// Car.prototype.accelerate = function(){
+//   this.speed +=10
+//   console.log(`${this.speed}`)
+// }
 
-Car.prototype.brake = function(){
-  this.speed -=5
-  console.log(`${this.speed}`)
-}
+// Car.prototype.brake = function(){
+//   this.speed -=5
+//   console.log(`${this.speed}`)
+// }
 
 
 // const versions =[1,2,3,4,5]
